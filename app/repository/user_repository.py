f7662ae7b75114d0d0d5f52e4a80f0aef46a1f0d
Gustavo Valle <gustavo.vallerp@hotmail.com>
2021-12-21 00:22:43 -0300

    find all and save methods

---
+++ app/repository/user_repository.py
@@ -0,0 +1,15 @@
+from app.database.config import SessionLocal
+from sqlalchemy.orm import Session
+from app.database import models
+from app.database.schemas import UserBase
+
+
+def find_all(db: Session):
+    return db.query(models.User).all()
+
+
+def save(db: Session, user: UserBase):
+    user = models.User(email=user.email, password=user.password)
+    db.add(user)
+    db.commit()
+    return user
