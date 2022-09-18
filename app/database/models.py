0dfe5a1ef07966c077f9c8eee60f32e73ee0cdbc
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-09-18 11:18:29 -0300

    :hammer: typing attributes

--- app/database/models.py
+++ app/database/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Integer, Column, String, Boolean
 from .config import Base
 
 
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255))
+    id: int = Column(Integer, primary_key=True, index=True)
+    email: str = Column(String(255), unique=True, nullable=False)
+    password: str = Column(String(255))
+    active: bool = Column(Boolean(), default=True)
